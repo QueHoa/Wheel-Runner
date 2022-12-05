ae5afe25cec8045affb8cc8a49051cce78cbe30e
Nguyễn Hoàng Quế Hòa <nhoxcoy2001.pdl@gmail.com>
2022-12-05 17:03:24 +0700

    add my project

--- Wheel Runner/Assets/Script/Player.cs
+++ Wheel Runner/Assets/Script/Player.cs
@@ -7,8 +7,13 @@ public class Player : MonoBehaviour
     private CharacterController controller;
     private Vector3 direction;
     private int desiredLane = 0;//left = -1, middle = 0, right = 1
+    private Vector3 oldPosition;
+
+    public float gravity = -20;//trọng lực khi nhảy
     public float speed;
     public float laneDistance = 4;//khoảng cách giữa hai làn đường
+    public float jumpPower;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +23,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction.z = speed;
+        direction.z = speed;        
+        controller.Move(direction * Time.deltaTime);
+
+        if (controller.isGrounded)
+        {
+            direction.y = -1;
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                Jump();
+            }
+        }
+        else
+        {
+            direction.y += gravity * Time.deltaTime;
+        }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             desiredLane++;
@@ -28,21 +47,22 @@ public class Player : MonoBehaviour
         {
             desiredLane--;
             if (desiredLane == -2) desiredLane = -1;
-        } 
+        }       
 
         Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
         
         if (desiredLane == -1)
-        {
+        { 
             targetPosition += Vector3.left * laneDistance;
         }else if (desiredLane == 1)
         {
             targetPosition += Vector3.right * laneDistance;
         }
+
         transform.position = Vector3.Lerp(transform.position, targetPosition, 50 * Time.deltaTime);
     }
-    private void FixedUpdate()
+    private void Jump()
     {
-        controller.Move(direction * Time.fixedDeltaTime);//nhân vật di chuyển
+        direction.y = jumpPower;
     }
 }
